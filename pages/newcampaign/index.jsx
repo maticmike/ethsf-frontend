6526deec297a2b968c37106e34773c0e03dc09d0
Ben Weinberg <weinbergben33@gmail.com>
2021-02-22 22:33:17 -0500

    added new campaign page

---
+++ pages/newcampaign/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Grid, Paper, TextField } from '@material-ui/core';
+import styles from './index.module.css';
+const NewCampaign = () => {
+  return (
+    <div className={styles.NewCampaign_box_positioning}>
+      <Paper className={styles.NewCampaign_layout} elevation={3}>
+        <div>
+          <h3>hello</h3>
+        </div>
+      </Paper>
+    </div>
+  );
+};
+
+export default NewCampaign;
