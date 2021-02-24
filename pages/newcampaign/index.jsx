ce6b810ee9cd467d9d968c3c14b360223113c191
Ben Weinberg <weinbergben33@gmail.com>
2021-02-23 22:34:08 -0500

    Added social media handle

--- pages/newcampaign/index.jsx
+++ pages/newcampaign/index.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Grid, Paper, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Paper, OutlinedInput, Grid } from '@material-ui/core';
+import CheckCircle from '@material-ui/icons/Done';
 import styles from './index.module.css';
+import Image from 'next/image';
 const NewCampaign = () => {
+  const [Influencer, setInfluencer] = useState('');
+  const findInfluencer = e => {
+    setInfluencer(e.target.value);
+  };
+
   return (
     <div className={styles.NewCampaign_box_positioning}>
       <Paper className={styles.NewCampaign_layout} elevation={3}>
-        <div>
-          <h3>hello</h3>
-        </div>
+        <Grid container column="column" align="center">
+          <Grid item sm={12}>
+            <OutlinedInput
+              id="time"
+              type="text"
+              placeholder="Find Influencer"
+              onChange={findInfluencer}
+              className={styles.NewCampaign_search_width}
+            />
+          </Grid>
+          <Grid item sm={5} align="right">
+            <Image src="/Instagram.png" alt={`${Influencer} Instagram Profile`} width="32" height="32" />
+          </Grid>
+          <Grid item sm={7} align="left">
+            <strong>@instagramProfile</strong>
+            <span>
+              <CheckCircle color="primary" />
+            </span>
+          </Grid>
+        </Grid>
       </Paper>
     </div>
   );
