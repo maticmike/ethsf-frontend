180dd40c3e1c76de9b29b56f2656024f6e3b2299
Ben Weinberg <weinbergben33@gmail.com>
2021-03-09 22:40:10 -0500

    Calendar added

--- components/newcampaign/CampaignDates/index.jsx
+++ components/newcampaign/CampaignDates/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.css';
 import { setObjectiveName } from '../../../utils/objectiveNames';
-
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 const CampaignDates = props => {
+  const [date, setDate] = useState(new Date());
+  const getHeading = () => (props.objective === 'singlePost' ? 'Post Date And Time' : 'Campaign Length');
+
   return (
-    <div>
+    <div className={styles.CampaignDates_layout}>
       <h2 className={styles.CampaignDates_custom_font}>{setObjectiveName(props.objective)} Objective</h2>
+      <p className={styles.CampaignDates_heading_font_size}>1. {getHeading()}</p>
+      <p className={styles.CampaignDates_helper_font}>
+        Select the range of dates which you want the campaign to last for
+      </p>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={date}
+          // onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </MuiPickersUtilsProvider>
     </div>
   );
 };
