c7f8b4fd2082e737237585b2d0d0204c733c2deb
Ben Weinberg <weinbergben33@gmail.com>
2021-03-24 21:50:33 -0400

    calendar font size increase css configured

--- pages/reviewcampaign/index.jsx
+++ pages/reviewcampaign/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import { Button } from '@material-ui/core';
 import { useStyles } from '../../styles/stylesReviewCampaign';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 const BusinessReviewHeader = dynamic(() => import('../../components/reviewcampaign/BusinessReviewHeader'), {
   loading: () => <p>Business Header Loading...</p>,
 });
@@ -16,9 +20,29 @@ const ReviewCampaign = () => {
         <div className={classes.ReviewCampaign_business_header}>
           <BusinessReviewHeader />
         </div>
+        <div className={classes.ReviewCampaign_vertical_line}></div>
         <div className={classes.ReviewCampaign_influencer_header}>
           <InfluencerReviewHeader />
         </div>
+      </div>
+      <Calendar
+        // onChange={handleDateChange}
+        // minDate={new Date()}
+        // selectRange={objective != 'singlePost' ? true : false}
+        // value={simpleDate}
+        className={classes.ReviewCampaign_calendar_size}
+      />
+      <br />
+      <div className={classes.ReviewCampaign_headers_side_by_side}>
+        <Button variant="contained" color="secondary" size="large">
+          Reject
+        </Button>
+        <Button variant="contained" color="primary" size="large">
+          Counter
+        </Button>
+        <Button variant="contained" color="primary" size="large">
+          Accept
+        </Button>
       </div>
     </>
   );
--- styles/stylesReviewCampaign.js
+++ styles/stylesReviewCampaign.js
@@ -3,12 +3,22 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   ReviewCampaign_headers_side_by_side: {
     display: 'flex',
+    justifyContent: 'center',
   },
   ReviewCampaign_influencer_header: {
     marginLeft: '55px',
   },
   ReviewCampaign_business_header: {
     marginRight: '55px',
+  },
+  ReviewCampaign_calendar_size: {
+    '& button': {
+      fontSize: '18px',
+    },
+  },
+  ReviewCampaign_vertical_line: {
+    borderLeft: '2px solid black',
+    height: '250px',
   },
 });
 export { useStyles };
