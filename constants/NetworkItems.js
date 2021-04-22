a52504dc2fa936ef0a129c1ab6f106b341cc38ff
Ben Weinberg <weinbergben33@gmail.com>
2021-04-21 22:12:27 -0400

    added basic web3 configuration

---
+++ constants/NetworkItems.js
@@ -0,0 +1,7 @@
+export const NETWORK_ID = 4;
+// export const GRAPH_API_URL = 'https://api.thegraph.com/subgraphs/name/scarcitylabs/openheco-rinkeby'; // Rinkeby example
+// export const EXPLORER_URL = 'https://hecoinfo.com/';
+// export const BASE_URL = 'https://opennft.io/';
+// export const META_IMG = `${BASE_URL}logo-text-under-large.png`;
+// export const CONTRACT_ADDRESS = '0xB41E6759B19da08115F464230a299Cb2799D1314';
+// export const PROJECT_APPLICATION_FORM_URL = 'https://jinshuju.net/f/SBuz52';
--- utils/web3/index.js
+++ utils/web3/index.js
@@ -1,10 +1,9 @@
 import { ethers } from 'ethers';
 import axios from 'axios';
-import FamepayFactory from '../../contracts/FamepayFactory.json';
-import FamepayJson from '../../contracts/Famepay.json';
 import consola from 'consola';
+import FamepayFactoryAbi from '../../contracts/FamepayFactory.json';
+import FamepayAbi from '../../contracts/Famepay.json';
 import { CONTRACT_RESPONSE_STATUS } from '../../constants/ContractResponse';
-export { bootstrapWeb3, getAccountInfo };
 
 /**
  * Response handler for successful contract interactions
@@ -50,4 +49,71 @@ const handleError = (functionName, error) => {
   };
 };
 
-export { handleResponse, handleError };
+/**
+ * Get contract address from abi
+ * @function getContractAddress
+ * @param abi contract abi
+ * @param chainId the network id
+ * @returns address of contract
+ */
+const getContractAddress = (abi, chainId) => {
+  if (!abi || !chainId) return '';
+  const { networks } = abi;
+  if (networks[chainId.toString()]) {
+    const address = networks[chainId.toString()].address;
+    return address || '';
+  }
+  return '';
+};
+
+/**
+ * Bootstrap web3
+ * @function bootstrapWeb3
+ * @returns the full contract objects with their addresses
+ */
+const bootstrapWeb3 = async () => {
+  try {
+    const provider = new ethers.providers.Web3Provider(web3.currentProvider);
+    const signer = provider.getSigner();
+    const network = await provider.getNetwork();
+    if (chain.chainId != NETWORK_ID) {
+      return false;
+    } else {
+      const famepayFactoryAddress = getContractAddress(FamepayFactoryAbi, network.chainId);
+      const famepayFactory = new ethers.Contract(famepayFactoryAddress, FamepayFactory.abi, signer);
+
+      const famepayAddress = getContractAddress(FamepayAbi, network.chainId);
+      const famepay = new ethers.Contract(famepayAddress, FamepayAbi.abi, signer);
+
+      return { famepayFactory, famepay };
+    }
+  } catch (error) {
+    conosola.error('bootstrapWeb3() error:', error);
+    return null;
+  }
+};
+
+/**
+ * Get eth wallet info
+ * @function getEthWallet
+ * @returns wallet and wallet related info
+ */
+const getWalletInfo = async () => {
+  try {
+    // const onboard = onBoardInitialize();
+    // await onboard.walletSelect();
+    // await onboard.walletCheck()
+    const provider = new ethers.providers.Web3Provider(web3.currentProvider);
+    const accounts = await provider.listAccounts();
+    const account = accounts && accounts[0];
+    const balanceRaw = await provder.getBalance(raw);
+    const balance = balanceRaw.toString();
+    const isAdmin = await marketplace.hasAdminRole(account);
+    return { account, balance, isAdmin };
+  } catch (error) {
+    consola.error('getWalletInfo() error:', error);
+    return null;
+  }
+};
+
+export { handleResponse, handleError, bootstrapWeb3, getWalletInfo };
