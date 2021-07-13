6cf02f2e6453d252e5d8b33dd680a1f2c865823e
Ben Weinberg <weinbergben33@gmail.com>
2021-07-12 21:56:43 -0400

    cleaned up twitter api code

--- pages/api/twitter/[tweet].js
+++ pages/api/twitter/[tweet].js
@@ -7,19 +7,6 @@ var client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-//Link Clicked, Tweet Viewed, and Profile Clicks --> private
-
-// const getData = async () => {
-//   const data = await axios.get(`http://localhost:4000/dev/getTweet/1383842980973277186`, {
-//     tweetId: 1383842980973277186,
-//   });
-//   console.log(data);
-// };
-
-// const cors = Cors({
-//   methods: ['GET', 'HEAD'],
-// });
-
 export default async (req, res) => {
   try {
     const tweetData = await client.get('tweets', {
@@ -33,39 +20,3 @@ export default async (req, res) => {
     console.log('Error On getTweetInfo():', error);
   }
 };
-
-// const getTweetData = async tweetId => {
-//   try {
-//     const getTweetData = await getTweetInfo(tweetId);
-//     const response = {
-//       statusCode: 200,
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify(getTweetData),
-//     };
-//     return response;
-//   } catch (err) {
-//     console.log('getTweetInfo error:', err);
-//     const response = {
-//       statusCode: err.statusCode || 500,
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({
-//         error: err.name,
-//         message: err.message,
-//       }),
-//     };
-//     return response;
-//   }
-// };
-
-// export const getPostData = postId => {
-//   if (postId.includes('twitter.com' && '/status/')) {
-//     let tweetId;
-//     if (postId.substr(postId.length - 4) === 's=20') {
-//       const tweet = postId.slice(0, -5);
-//       tweetId = tweet.substr(postId.length - 19);
-//     } else {
-//       tweetId = postId.substr(postId.length - 19);
-//     }
-//     getTweetData(tweetId);
-//   }
-// };
--- services/twitter/twitter.js
+++ services/twitter/twitter.js
@@ -44,28 +44,4 @@ export const getPostData = postId => {
     }
     getTweetData(tweetId);
   }
-  //if facebook
-  //if youtube
 };
-
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, result => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
-async function handler(req, res) {
-  // Run the middleware
-  await runMiddleware(req, res, cors);
-
-  // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' });
-}
-
-export default handler;
